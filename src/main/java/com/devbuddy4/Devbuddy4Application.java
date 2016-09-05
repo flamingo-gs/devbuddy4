d7df8088eec5f1cba98aa7c99eae8afcdfe8082e
Ahmed Fadhel <flamingo.gs@gmail.com>
2016-09-05 17:30:02 +0300

    initial commit all

---
+++ src/main/java/com/devbuddy4/Devbuddy4Application.java
@@ -0,0 +1,12 @@
+package com.devbuddy4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devbuddy4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devbuddy4Application.class, args);
+	}
+}
